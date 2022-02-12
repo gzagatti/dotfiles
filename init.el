863557e08a76985167cea29fc673cb67bda58459
gzagatti <gzagatti@users.noreply.github.com>
2022-02-12 13:59:29 +0800

    more tweaks to emacs conf.

--- init.el
+++ init.el
@@ -1,13 +1,11 @@
 ; Basic Settings {{{
 
-; do not follow symlinks
-(setq vc-follow-symlinks nil)
-
 ;; importing settings from literate config {{{
 (require 'ob-tangle)
+; do not follow symlinks
+(setq vc-follow-symlinks nil)
+; default header arguments
 ; https://org-babel.readthedocs.io/en/latest/header-args/
 (add-to-list 'org-babel-default-header-args '(:tangle . "yes"))
 (org-babel-load-file (expand-file-name "emacs-config.org" user-emacs-directory))
 ;;}}}
-
-;}}}
