4680e69c9784f3cd13e1929cb5db0fd7343b6bc7
gzagatti <gzagatti@users.noreply.github.com>
2022-11-03 14:28:00 +0800

    opens file vertically.

--- nvim/init.lua
+++ nvim/init.lua
@@ -91,6 +91,24 @@ require'packer'.startup {function (use)
               vim.api.nvim_set_current_win(nnnwin)
             end
           },
+          {"<C-v>", function (files)
+              local nnnwin
+              for _, win in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+                if vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(win), "filetype") == "nnn" then
+                  nnnwin = win
+                  break
+                end
+              end
+              for i, file in ipairs(files) do
+                if i == 1 and vim.api.nvim_buf_get_name(0) == "" then
+                  vim.cmd("edit "..file)
+                else
+                  vim.cmd("vsplit "..file)
+                end
+              end
+              vim.api.nvim_set_current_win(nnnwin)
+            end
+          },
           -- cd to file directory
           {"<C-w>", builtin.cd_to_path },
         },
