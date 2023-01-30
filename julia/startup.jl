9b96d1398f5d929d1241b0b9e617df2bbc4db1e5
gzagatti <gzagatti@users.noreply.github.com>
2023-01-30 15:18:21 +0800

    fix colors in Julia REPL when using leuven.

--- julia/startup.jl
+++ julia/startup.jl
@@ -1,19 +1,34 @@
 import Pkg
 # activate project at current path or at repo root
-if isfile("Project.toml") && 
+if isfile("Project.toml") &&
   isfile("Manifest.toml")
     Pkg.activate(".")
 else
   here = splitpath(abspath("."))
-  for i in length(here):-1:1
-    parent = here[1:i]
+  for level in length(here):-1:1
+    parent = here[1:level]
     if isdir(joinpath([parent; ".git"]))
-      if isfile(joinpath([parent; "Project.toml"])) && 
+      if isfile(joinpath([parent; "Project.toml"])) &&
         isfile(joinpath([parent; "Manifest.toml"]))
         Pkg.activate(joinpath(parent))
         break
       end
     end
-    if i == 1 Pkg.activate() end
+    if level == 1 Pkg.activate() end
   end
 end
+
+let theme = get(ENV, "THEME", "")
+  if theme == "leuven"
+    # colors are sourced from Base.text_colors
+    # see: https://github.com/JuliaLang/julia/blob/master/base/client.jl
+    ENV["JULIA_ERROR_COLOR"] = 1
+    ENV["JULIA_WARN_COLOR"] = 3
+    function leuven_colors(repl)
+      repl.shell_color = Base.text_colors[5]
+    end
+    atreplinit(leuven_colors)
+  end
+end
+
+
