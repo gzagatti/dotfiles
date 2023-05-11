7602c46c85df67f8a3ab10693bc3acd71096105e
gzagatti <gzagatti@users.noreply.github.com>
2023-05-11 19:05:10 +0800

    tweak kittens for smoother navigation on remote server

--- kitty/imgviewer.py
+++ kitty/imgviewer.py
@@ -3,11 +3,15 @@ from pathlib import Path
 from kitty.boss import Boss
 from kittens.tui.handler import result_handler
 
+
 def main(args: List[str]) -> str:
     pass
 
+
 @result_handler(no_ui=True)
-def handle_result(args: List[str], result: None, target_window_id: int, boss: Boss) -> None:
+def handle_result(
+    args: List[str], result: None, target_window_id: int, boss: Boss
+) -> None:
     args = args[1:]
     files = []
     w = boss.window_id_map.get(target_window_id)
@@ -20,8 +24,9 @@ def handle_result(args: List[str], result: None, target_window_id: int, boss: Bo
     try:
         imgviewer = next(boss.match_windows(f"title:imgviewer{w.id}"))
     except StopIteration:
-        imgviewer = boss.launch("--window-title", f"imgviewer{w.id}",
-            "--cwd", "current", "--keep-focus")
+        imgviewer = boss.launch(
+            "--window-title", f"imgviewer{w.id}", "--cwd", "current", "--keep-focus"
+        )
         imgviewer = next(boss.match_windows(f"title:imgviewer{w.id}"))
         imgviewer.write_to_child("export HISTFILE=\r")
     imgviewer.write_to_child(f"timg {' '.join(str(f) for f in files)}\r")
