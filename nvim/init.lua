a4705ff2edc62250878aba6bd18a3e2163c5bfd8
gzagatti <gzagatti@users.noreply.github.com>
2023-08-22 10:44:20 +0800

    Yet another tweak to command-line window mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1380,6 +1380,7 @@ vim.cmd [[
   au TermOpen * setlocal nonumber
 ]]
 ---}}}
+
 --}}}
 
 --key mappings {{{
@@ -1533,6 +1534,16 @@ vim.api.nvim_set_keymap('n', '/', '/\\v', { noremap = true })
 vim.api.nvim_set_keymap('n', '?', '?\\v', { noremap = true })
 ---}}}
 
+--- command-line window {{{
+-- maps for the command-line window get on the way of quitting Vim
+vim.api.nvim_set_keymap('n', 'q:', '', { nowait = true, noremap = true })
+vim.api.nvim_set_keymap('n', 'q/', '', { nowait = true, noremap = true })
+vim.api.nvim_set_keymap('n', 'q?', '', { nowait = true, noremap = true })
+vim.api.nvim_set_keymap('n', 'c:', 'q:', { noremap = true })
+vim.api.nvim_set_keymap('n', 'c/', 'q/', { noremap = true })
+vim.api.nvim_set_keymap('n', 'c?', 'q?', { noremap = true })
+---}}}
+
 --}}}
 
 --cmd {{{
@@ -1587,8 +1598,8 @@ vim.cmd [[
     "configuration files
     autocmd BufNewFile,BufRead *.*rc,*rc,init.lua setlocal foldmethod=marker
 
-    "leave the command-line window with q
-    autocmd CmdwinEnter * map <buffer> q <c-c>q
+    "leave the command-line window
+    autocmd CmdwinEnter * nmap <buffer> <nowait> q :quit<cr>
 
     "rmd
     " adds vim-markdown as a filetype plugin in order to allow
